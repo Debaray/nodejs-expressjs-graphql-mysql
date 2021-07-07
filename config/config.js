e42c54838b355c6157ea22c5e06366f71e217e42
Bysl-soft-09 <debaray14@gmail.com>
2021-07-07 18:56:30 +0600

    design user model

--- config/config.js
+++ config/config.js
@@ -1,9 +1,11 @@
-{
+require('dotenv').config();
+
+module.exports = {
   "development": {
-    "username": "root",
-    "password": "Debashis@123",
-    "database": "testdb",
-    "host": "localhost",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
   "test": {
