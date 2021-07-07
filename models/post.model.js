539bb11e63bf8c19ae4551b142b05cbd00aa17e3
Bysl-soft-09 <debaray14@gmail.com>
2021-07-07 19:16:35 +0600

    design post  model

---
+++ models/post.model.js
@@ -0,0 +1,30 @@
+module.exports = (sequelize, DataTypes) =>{
+    const Post = sequelize.define('Post',{
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        status: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        }
+    },{
+        paranoid: true,
+        timestamps: true,
+        deleteAt: 'deleteAT',
+    });
+    Post.associate = function(models){
+        Post.belongsTo(models.User)
+    };
+    return Post;
+}
